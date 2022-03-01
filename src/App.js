f81f23a8a07567ba3cf9588c4b7a7249475775bf
farhana7 <farhanaadf@gmail.com>
2022-03-01 18:50:13 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import Home from "./Components/Home/Home/Home";
+import Services from "./Components/Home/Services/Services";
+import TourGuides from "./Components/Home/TourGuides/TourGuides";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/services">
+            <Services></Services>
+          </Route>
+          <Route path="/tourguides">
+            <TourGuides></TourGuides>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Home/TourGuides/TourGuides.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TourGuides = () => {
+  return (
+    <div>
+      <h2>our Tour Guides</h2>
+    </div>
+  );
+};
+
+export default TourGuides;
